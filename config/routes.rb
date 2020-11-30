790341a2dfdc8b8e2db9dbb9a60b508724476ca9
mathieu fauvette <fauvettemathieu.mf@gmail.com>
2020-11-30 18:45:32 +0100

    routes and restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :meals, :tables
+  end
+
+  resources :customers do
+    resources :tables, :orders
+  end
+
 end
