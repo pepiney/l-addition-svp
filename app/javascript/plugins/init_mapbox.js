5d58bf253dc03e7e1c14b57af265c5b7b4087caf
mathieu fauvette <fauvettemathieu.mf@gmail.com>
2020-12-07 09:32:53 +0100

    gecoding

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,33 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const buildMap = (mapElement) => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/pepiney/ckiea27wj1tb619rwjmpt65bu'
+  });
+};
+
+const addMarkerToMap = (map, marker) => {
+  new mapboxgl.Marker()
+  .setLngLat([ marker.lng, marker.lat ])
+  .addTo(map);
+};
+
+const fitMapToMarker = (map, marker) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  bounds.extend([ marker.lng, marker.lat ]);
+  map.fitBounds(bounds, { padding: 50, maxZoom: 13, duration: 0 });
+};
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    const map = buildMap(mapElement);
+    const marker = JSON.parse(mapElement.dataset.marker);
+    addMarkerToMap(map, marker);
+    fitMapToMarker(map, marker);
+  }
+};
+export { initMapbox };
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,6 @@ class Restaurant < ApplicationRecord
   has_many :tables, dependent: :destroy
   validates :name, :address, presence: true
   # validates :description, :photo, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
